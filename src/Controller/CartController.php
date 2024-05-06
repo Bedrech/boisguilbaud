4ad8fe385928e7633bf57839b801769bd5d181cb
Bedrech <jr17000@live.fr>
2024-05-06 13:42:35 +0200

    oui

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -206,7 +206,7 @@ public function sendEmail(
     // Créer l'e-mail
     $email = (new Email())
         ->from($email)
-        ->to('boisguilbaud@gmail.com')
+        ->to('jcousin015@gmail.com')
         ->subject("Commande de $nom")
         ->html($contenuEmail);
 
